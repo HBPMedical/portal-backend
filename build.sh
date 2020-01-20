05742119e178e7552d61ea10f6d7c7ec85f4bb98
JerryPan44 <jerrypan44@gmail.com>
2020-01-20 10:52:38 +0000

    get ready for merge

--- build.sh
+++ build.sh
@@ -34,9 +34,9 @@ docker build --build-arg BUILD_DATE=$(date -Iseconds) \
     --build-arg VCS_REF=$VCS_REF \
     --build-arg VERSION=$VERSION \
     --tag "$IMAGE:latest" \
-    --tag "jerrypan44/portal-backend:latest" \
+    --tag "$IMAGE:$VERSION" \
     .
-docker push "jerrypan44/portal-backend:latest"
+
 
 BUGSNAG_KEY=""
 eval $(grep -e "^\\s*BUGSNAG_KEY" Dockerfile | tr '\\' ' ')

6a75260e9d224c33447b6246065ef0da887d2e62
ThanKarab <tkarabatsis@hotmail.com>
2020-10-23 13:08:02 +0300

    Reduced logs and removed spacing.

--- src/main/java/eu/hbp/mip/utils/Logging.java
+++ src/main/java/eu/hbp/mip/utils/Logging.java
@@ -11,9 +11,7 @@ public class Logging {
 
     public static void LogUserAction(String userName, String endpoint, String actionInfo) {
         LOGGER.info(" User -> " + userName + " ,"
-                + calculateAdditionalSpacing(userName.length(), 8)
                 + "Endpoint -> " + endpoint + " ,"
-                + calculateAdditionalSpacing(endpoint.length(), 32)
                 + "Info ->  " + actionInfo);
     }
 
@@ -21,20 +19,6 @@ public class Logging {
     public static void LogExperimentAction(String experimentName, UUID experimentId, String actionInfo) {
         LOGGER.info(" Experiment -> " + experimentName
                 + "(" + experimentId + ") ,"
-                + calculateAdditionalSpacing(experimentName.length() + experimentId.toString().length() + 2, 20)
                 + "Info -> " + actionInfo);
-    }
-
-    // Used when a user is not authorised yet
-    public static void LogAction(String actionName, String actionIdInfo) {
-        LOGGER.info(" Action -> " + actionName + " ,"
-                + calculateAdditionalSpacing(actionName.length() + 2, 20)
-                + "Info -> " + actionIdInfo);
-    }
-
-    // Calculates the spacing that is needed to create consistent logs.
-    private static String calculateAdditionalSpacing(Integer currentLen, Integer maxLen) {
-        int additionalSpacing = (maxLen > currentLen ? maxLen - currentLen + 2 : 2);
-        return String.format("%" + additionalSpacing + "s", "");
     }
 }

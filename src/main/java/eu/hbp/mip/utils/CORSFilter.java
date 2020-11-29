95dfe1d7bafbb7ed083a0faa19ee565905ade865
ThanKarab <tkarabatsis@hotmail.com>
2020-11-29 04:42:10 -0800

    Fix for CORS allowed requests.

--- src/main/java/eu/hbp/mip/utils/CORSFilter.java
+++ src/main/java/eu/hbp/mip/utils/CORSFilter.java
@@ -16,7 +16,7 @@ public class CORSFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Request-Headers", "*");

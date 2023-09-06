c4461abe0e78b720cd83fb410d66f9692a2d025f
kfilippopolitis <kostasfilippop@gmail.com>
2023-09-06 15:56:44 +0300

    JPA Data no longer get exposed as endpoints.

--- src/main/java/hbp/mip/MIPApplication.java
+++ src/main/java/hbp/mip/MIPApplication.java
@@ -7,8 +7,9 @@ package hbp.mip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 public class MIPApplication {
 
     public static void main(String[] args) {

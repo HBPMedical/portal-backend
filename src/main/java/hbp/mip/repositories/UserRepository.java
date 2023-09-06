240a0e70340b61253e1d0954abad3ce328569c17
kfilippopolitis <kostasfilippop@gmail.com>
2023-09-06 17:10:56 +0300

    JPA Data no longer get exposed as endpoints.

--- src/main/java/hbp/mip/repositories/UserRepository.java
+++ src/main/java/hbp/mip/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package hbp.mip.repositories;
 
 import hbp.mip.models.DAOs.UserDAO;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RestResource(exported = false)
 public interface UserRepository extends CrudRepository<UserDAO, String> {
     UserDAO findByUsername(String username);
 }
